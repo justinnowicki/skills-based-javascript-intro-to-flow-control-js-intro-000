8ed99288a2a8ca408a95afb4e4af39506a81eb6b
Justin Nowicki <justinnowicki@gmail.com>
2019-05-25 11:21:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,6 +18,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    console.log("You are a teenager!")
+  }
+  else if (age < 13) {
+    console.log("You are a kid")
+  }
+  else {
+    console.log("You are a grownup")
+  }
 
 }
 
